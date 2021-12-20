5e0c2ea4673f2b782b69a2f2c7fd333e6fb00621
Beltran Rengifo <beltran.rengifo@liferay.com>
2021-12-20 09:29:48 +0100

    v1 test version

---
+++ index.js
@@ -0,0 +1,40 @@
+const http = require('axios')
+const { USER, TOKEN } = require('./secrets')
+
+const WOFFU_CONFIG = {
+  url: 'https://liferay.woffu.com/api/svc/signs/signs',
+  data: {
+    UserId: USER,
+    DeviceId: 'WebApp'
+  },
+  headers: {
+    Authorization: `Bearer ${TOKEN}`,
+    'Content-Type': 'application/json;charset=utf-8'
+  }
+}
+
+const WoffuClient = http.create({
+  baseURL: WOFFU_CONFIG.url,
+  timeout: 3000,
+  headers: WOFFU_CONFIG.headers
+});
+
+console.log('hereeee')
+
+/* WoffuClient.post('/', WOFFU_CONFIG.data)
+  .then(({
+    status,
+    statusText,
+    data: { signEventId } = {}
+  } = {}) => {
+    console.log({
+      'Request Status': status,
+      'Request Status Text': statusText,
+      'Sign-in event Id': signEventId
+    })
+  })
+  .catch(error => {
+    console.warn(error)
+  })
+
+ */
